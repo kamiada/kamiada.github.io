64cb6a3af5018210802282f98ac8fc3e8031d9cf
kamiada <kamiada528@gmail.com>
2022-03-26 21:13:51 +0000

    responsive view

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -4,28 +4,25 @@ import "./components.scss";
 class Tile extends Component {
   render() {
     return (
-      <div className="tile_container">
-        <div className="main_tile">
+      <div className="tile_parent">
+        <div className="tile_text_container">
           <span className="company">{this.props.company}</span>
           <span className="job_title">{this.props.job_title}</span>
           <span>{this.props.grade}</span>
           <span>{this.props.final_project}</span>
           {this.props.points &&
             this.props.points.map((point, key) => (
-              <ul>
-                <li key={key}>{point}</li>
-              </ul>
+              <p className="description_container" key={key}>
+                {point}
+              </p>
             ))}
           {this.props.skills &&
-            this.props.skills.map((skill) => <span>Used technologies: {skill}</span>)}
+            this.props.skills.map((skill) => (
+              <span>Used technologies: {skill}</span>
+            ))}
         </div>
-
-        <div className="tile_image_container">
-          <img
-            className="tile_image"
-            src={this.props.image}
-            alt={this.props.alt}
-          />
+        <div className="tile_img_container">
+          <img src={this.props.image} alt={this.props.alt} />
         </div>
       </div>
     );
--- src/pages/about_me/index.js
+++ src/pages/about_me/index.js
@@ -8,27 +8,38 @@ const aboutMe = () => {
     <Layout>
       <div id="about" className="page_container">
         <div className="padding_between_pages">
-        <div className="section_title--aboutMe">About me</div>
-        <div className="exact_container"> 
-        <div className="picture_container">
-            <img src={photo} alt="Dynamic earth" />
+          <div className="section_title--aboutMe">About me</div>
+          <div className="exact_container">
+            <div className="picture_container">
+              <img src={photo} alt="Dynamic earth" />
+            </div>
+            <div className="text_container">
+              <br />
+              <p>
+                I work as a frontend mobile engineer for Stint. My hobbies
+                includes:
+                <ul>
+                  <li>
+                    Programming games and building side-projects in Unity, React
+                    and Python{" "}
+                  </li>
+                  <li>
+                    Reading books - I like popular science, fantasy and
+                    science-fiction and classics like Bułhakow and Dostoyevsky
+                  </li>
+                  <li>Learning new things</li>
+                  <li>Learning and reading about history</li>
+                  <li>Studying French</li>
+                </ul>
+                My interests are focused on the impact of technology on people's
+                everyday life and how technology can improve things.
+                <br />
+                <br />
+                To find out more about me scroll down 😊
+                <br />
+              </p>
+            </div>
           </div>
-          <div className="text_container">
-            <p>
-              I work as a frontend mobile engineer for Stint.
-              In my free time, I like to program in Unity, React and Python for fun, 
-              read books, learning French and history.
-              <br />
-              <br />
-              My interests are focused on the impact of technology on people's
-              everyday life and how technology can improve things.
-              <br />
-              <br />
-              To find out more about me scroll down 😊
-              <br />
-            </p>
-          </div>
-        </div>
         </div>
       </div>
     </Layout>
