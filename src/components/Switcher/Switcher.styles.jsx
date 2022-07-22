5fa12863036ee012e93311310a57cb06a2833fd6
kamiada <kamiada528@gmail.com>
2022-07-22 21:00:53 +0100

    working with styling

--- src/components/Switcher/Switcher.styles.jsx
+++ src/components/Switcher/Switcher.styles.jsx
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 10em;
+  position: -webkit-sticky;
+  position: sticky;
+  left: 0;
+  top: 0;
+  width: 100%;
+  top: 10px;
+  z-index: 5;
+  float: right;
 `;
 export const SwitchButton = styled.button`
   color: #fff;
@@ -11,4 +19,10 @@ export const SwitchButton = styled.button`
   background: transparent;
   font-family: 'Libre Baskerville', serif;
   font-size: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: "Source Sans Pro", sans-serif;
+  display: inline;
+  cursor: pointer;
+  color: black;
 `;
