3319ff66a93df4dddcd1366994910ee71522e532
kamiada <kamiada528@gmail.com>
2023-01-18 20:43:57 +0000

    nice, menu starts looking nicer

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ import "./components.scss";
 
 const Menu = () => {
   const showMenu = () => {
-    let topNav = document.getElementById('topNav');
+    let topNav = document.getElementById("topNav");
     if (topNav.style.display === "block") {
       topNav.style.display = "none";
     } else {
@@ -19,27 +19,27 @@ const Menu = () => {
       <div className="topNav" id="topNav">
         <nav className="navbar">
           <ul>
-            <li>
+            <li id="links">
               <Link activeClass="active" to="home" spy={true} smooth={true}>
                 .home
               </Link>
             </li>
-            <li>
+            <li id="links">
               <Link to="about" spy={true} smooth={true}>
                 .about me
               </Link>
             </li>
-            <li>
+            <li id="links">
               <Link to="work" spy={true} smooth={true}>
                 .work
               </Link>
             </li>
-            <li>
+            <li id="links">
               <Link to="projects" spy={true} smooth={true}>
                 .projects
               </Link>
             </li>
-            <li>
+            <li id="links">
               <Link to="contact" spy={true} smooth={true}>
                 .contact
               </Link>
