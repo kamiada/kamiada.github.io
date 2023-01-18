1efc3e040fe4dc12ea5cba18b2fb1d8ab255db64
kamiada <kamiada528@gmail.com>
2023-01-18 23:13:23 +0000

    made it all css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,50 +3,38 @@ import { Link } from "react-scroll";
 import "./components.scss";
 
 const Menu = () => {
-  const showMenu = () => {
-    let topNav = document.getElementById("topNav");
-    if (topNav.style.display === "block") {
-      topNav.style.display = "none";
-    } else {
-      topNav.style.display = "block";
-    }
-  };
   return (
     <div>
-      <button className="mobile_menu" onClick={() => showMenu()}>
-        Menu
-      </button>
-      <div className="topNav" id="topNav">
-        <nav className="navbar">
-          <ul>
-            <li id="links">
-              <Link activeClass="active" to="home" spy={true} smooth={true}>
-                .home
-              </Link>
-            </li>
-            <li id="links">
-              <Link to="about" spy={true} smooth={true}>
-                .about me
-              </Link>
-            </li>
-            <li id="links">
-              <Link to="work" spy={true} smooth={true}>
-                .work
-              </Link>
-            </li>
-            <li id="links">
-              <Link to="projects" spy={true} smooth={true}>
-                .projects
-              </Link>
-            </li>
-            <li id="links">
-              <Link to="contact" spy={true} smooth={true}>
-                .contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <input type="checkbox" className="mobile_menu" title="Menu"></input>
+      <nav id="navbar">
+        <ul>
+          <li id="links">
+            <Link activeClass="active" to="home" spy={true} smooth={true}>
+              .home
+            </Link>
+          </li>
+          <li id="links">
+            <Link to="about" spy={true} smooth={true}>
+              .about me
+            </Link>
+          </li>
+          <li id="links">
+            <Link to="work" spy={true} smooth={true}>
+              .work
+            </Link>
+          </li>
+          <li id="links">
+            <Link to="projects" spy={true} smooth={true}>
+              .projects
+            </Link>
+          </li>
+          <li id="links">
+            <Link to="contact" spy={true} smooth={true}>
+              .contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
