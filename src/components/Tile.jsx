682154bc71e2f92224738a47faa75981d0870c01
kamiada <kamiada528@gmail.com>
2023-01-24 17:34:07 +0000

    updating experience

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -5,42 +5,21 @@ import { Fragment } from "react/cjs/react.production.min";
 class Tile extends Component {
   render() {
     return (
-    //   <div className="tile_parent">
-    //     <div className="tile_text_container">
-    //     <div className="tile_img_container">
-    //       <img src={this.props.image} alt={this.props.alt} />
-    //     </div>
-    //       <span className="company">{this.props.company}</span>
-    //       <span className="job_title">{this.props.job_title}</span>
-    //       <span>{this.props.grade}</span>
-    //       <span>{this.props.final_project}</span>
-          // {this.props.points &&
-          //   this.props.points.map((point, key) => (
-          //     <p className="description_container" key={`${key}+${point}`}>
-          //       {point}
-          //     </p>
-          //   ))}
-          // {this.props.skills &&
-          //   this.props.skills.map((skill, key) => (
-          //     <span key={key}>Used technologies: {skill}</span>
-          //   ))}
-    //     </div>
-    //   </div>
-    // );
       <Fragment>
         <div className="img_holder">
           <img src={this.props.image} alt={this.props.alt} />
         </div>
         <div>
-        <span className="company">{this.props.company}</span>
+          <span className="company">{this.props.company}</span>
         </div>
-        <div>
-        <span className="job_title">{this.props.job_title}</span>
-        <span>{this.props.grade}</span>
-        <span>{this.props.final_project}</span>
+        <div className="tile_education_holder">
+          <span className="job_title">{this.props.job_title}</span>
+          <span>{this.props.grade}</span>
+          <span>{this.props.final_project}</span>
         </div>
-        <div>
-        {this.props.points &&
+
+        <div className="tile_description_holder">
+          {this.props.points &&
             this.props.points.map((point, key) => (
               <p className="description_container" key={`${key}+${point}`}>
                 {point}
